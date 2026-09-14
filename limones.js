ef67dfa9da2628a5d1aa344b3e802b11bca01234
mvcobos <vcobos178@gmail.com>
2026-09-14 15:10:58 -0500

    Agregar dibujarSuelo y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -1,2 +1,18 @@
 let canvas = document.getElementById("areaJuego");
-let ctx = canvas.getContext("");
+let ctx = canvas.getContext("2d");
+
+let alturaSuelo = 40;
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+function dibujarSuelo(){
+    ctx.fillStyle = "#FEE7C8";
+    ctx.fillRect(0,canvas.height - alturaSuelo, canvas.width,alturaSuelo);
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle = "#ff9292";
+    ctx.fillRect(canvas.width/2, canvas.height-(alturaSuelo+60), 40, 60);
+}
