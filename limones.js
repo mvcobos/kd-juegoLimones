39ee2ae854a2832c20a5ee118ccde2209b3aa0b2
mvcobos <vcobos178@gmail.com>
2026-09-18 17:54:58 -0500

    mover izquierda y derecha

--- limones.js
+++ limones.js
@@ -1,18 +1,45 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
-let alturaSuelo = 40;
+const ALTURA_SUELO = 40;
+const ALTURA_PERSONAJE = 60;
+const ANCHO_PERSONAJE = 40;
+let personajeX = canvas.width/2;
+let personajeY = canvas.height/2;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
 }
+
+// (x, y, ancho, alto)
 function dibujarSuelo(){
     ctx.fillStyle = "#FEE7C8";
-    ctx.fillRect(0,canvas.height - alturaSuelo, canvas.width,alturaSuelo);
+    ctx.fillRect(0,canvas.height - ALTURA_SUELO, canvas.width, ALTURA_SUELO);
 }
 
 function dibujarPersonaje(){
     ctx.fillStyle = "#ff9292";
-    ctx.fillRect(canvas.width/2, canvas.height-(alturaSuelo+60), 40, 60);
+    ctx.fillRect(personajeX, canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE), ANCHO_PERSONAJE, ALTURA_PERSONAJE);
+}
+
+function moverIzquierda(){
+    personajeX-=10;
+    actualizarPantalla();
+}
+
+function moverDerecha(){
+    personajeX+=10;
+    actualizarPantalla();
+}
+
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
 }
