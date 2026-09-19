2b1b9a5757069ef31b87d886f14a1ba0d2924f97
mvcobos <vcobos178@gmail.com>
2026-09-18 19:31:11 -0500

    Bajar limon y correccion de colores

--- limones.js
+++ limones.js
@@ -4,22 +4,28 @@ let ctx = canvas.getContext("2d");
 const ALTURA_SUELO = 40;
 const ALTURA_PERSONAJE = 60;
 const ANCHO_PERSONAJE = 40;
+const ALTURA_LIMON = 20;
+const ANCHO_LIMON = 20;
+
 let personajeX = canvas.width/2;
 let personajeY = canvas.height/2;
+let limonX = canvas.width/2;
+let limonY = 5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 // (x, y, ancho, alto)
 function dibujarSuelo(){
-    ctx.fillStyle = "#FEE7C8";
+    ctx.fillStyle = "#3DB6B1";
     ctx.fillRect(0,canvas.height - ALTURA_SUELO, canvas.width, ALTURA_SUELO);
 }
-
+//#ff9292
 function dibujarPersonaje(){
-    ctx.fillStyle = "#ff9292";
+    ctx.fillStyle = "#CC3A63";
     ctx.fillRect(personajeX, canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE), ANCHO_PERSONAJE, ALTURA_PERSONAJE);
 }
 
@@ -33,11 +39,22 @@ function moverDerecha(){
     actualizarPantalla();
 }
 
+function dibujarLimon(){
+    ctx.fillStyle = "#A8DF8E";
+    ctx.fillRect(limonX, limonY, ANCHO_LIMON, ALTURA_LIMON);
+    actualizarPantalla
+}
+
+function bajarLimon(){
+    limonY+=10;
+    actualizarPantalla();
+}
 
 function actualizarPantalla(){
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 
 function limpiarCanva(){
